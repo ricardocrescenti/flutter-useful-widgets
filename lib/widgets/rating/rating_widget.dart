33157d884c7e34ac2156965f39911d37ccedbdfd
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2021-01-28 08:16:25 -0300

    Added 'RatingWidget'

---
+++ lib/widgets/rating/rating_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RatingWidget extends StatelessWidget {
+	final int iconCount;
+	final int currentRating;
+	final double iconSize;
+	final Color markedColor;
+	final Color unmarkedColor;
+	final Widget Function(bool marked) iconBuilder;
+
+	RatingWidget({
+		@required this.iconCount,
+		@required this.currentRating,
+		this.iconSize = 15,
+		this.markedColor = Colors.grey,
+		this.unmarkedColor = Colors.orange,
+		this.iconBuilder,
+	});
+
+	@override
+	Widget build(BuildContext context) {
+		return Row(
+			children: _buildIcons(),
+		);
+	}
+
+	List<Widget> _buildIcons() {
+		List<Widget> icons = [];
+
+		for (var i = 0; i < iconCount; i++) {
+			bool marked = (currentRating <= i);
+			icons.add(iconBuilder != null 
+				? iconBuilder(marked) 
+				: _buildIcon(marked)
+			);
+		}
+
+		return icons;
+	}
+
+	Widget _buildIcon(bool marked) {
+		return Icon(Icons.star, 
+			size: iconSize,
+			color: (marked ? markedColor : unmarkedColor)
+		);
+	}
+		
+}
